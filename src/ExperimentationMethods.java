0ce6010ab77232efb467e233639568d47a06840a
aleql <harambedcc@gmail.com>
2017-10-02 01:18:03 -0300

    Agregados mas metodos de prueba

--- src/ExperimentationMethods.java
+++ src/ExperimentationMethods.java
@@ -4,12 +4,14 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class ExperimentationMethods {
     public static StringBuilder filesData (String path, StringBuilder sb) {
+        ArrayList<Double> results = new ArrayList<>();
         File dir = new File(path);
         File[] directoryListing = dir.listFiles();
         long numberOfFiles = 0L;
@@ -18,11 +20,16 @@ public class ExperimentationMethods {
                 numberOfFiles++;
                 double bytes = child.length();
                 double percentageOfUse = (bytes/4096);
-                sb.append(percentageOfUse);
-                sb.append("\n");
+                results.add(percentageOfUse);
+                child.delete();
             }
-            sb.append("Number of files: ");
-            sb.append(numberOfFiles);
+            double[] arr = results.stream().mapToDouble(i -> i).toArray();
+            Statistics stats = new Statistics(arr);
+            sb.append("Number of nodes: ").append(numberOfFiles).append("\n");
+            sb.append("Mean: ").append(stats.getMean()).append("\n");
+            sb.append("Median: ").append(stats.median()).append("\n");
+            sb.append("Variance: ").append(stats.getVariance()).append("\n");
+            sb.append("Standard deviation: ").append(stats.getStdDev()).append("\n");
         } else {
             System.out.println("Could not opern directory");
             System.exit(-1);
@@ -39,5 +46,31 @@ public class ExperimentationMethods {
             System.out.println("Could not write on file" + name);
             e.printStackTrace();
         }
+    }
+
+    public static void experiment (long N, String path, String name) {
+
+        long tStart = System.currentTimeMillis();
+
+        Rtree rtree1 = new Rtree(path);
+        Node l = new Node("L");
+        Node r = new Node("L");
+        NodeMethods.saveNode(l);
+        NodeMethods.saveNode(r);
+        Rtree.newRoot(l,r);
+        Rtree.splitter = new LinearSplit();
+        TestMethods.insertN(N);
+
+        long tEnd = System.currentTimeMillis();
+        long tDelta = tEnd - tStart;
+        double elapsedSeconds = tDelta / 1000.0;
+
+        int treeHeigth = TestMethods.treeHeigth();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Built tree: ").append(Rtree.treeId).append("\n");
+        sb.append("Time elapsed: ").append(elapsedSeconds).append("\n");
+        sb.append("Tree heigth: ").append(treeHeigth).append("\n");
+        ExperimentationMethods.resultsFile(name, path, ExperimentationMethods.filesData(path,sb));
     }
 }
--- src/TestResults.java
+++ src/TestResults.java
@@ -1,7 +1,9 @@
 public class TestResults {
     public static void main(String[] args) {
         String pathAle = "/home/alejandro/tareas/8-semestre/ln/t1/Tarea1Logaritmos/temp/";
-        ExperimentationMethods.resultsFile("resultados.txt", pathAle, ExperimentationMethods.filesData(pathAle,new StringBuilder()));
+        String name = "resultados.txt";
+        ExperimentationMethods.experiment(24,pathAle,name);
+        //ExperimentationMethods.resultsFile("resultados.txt", pathAle, ExperimentationMethods.filesData(pathAle,new StringBuilder()));
     }
 
 }
